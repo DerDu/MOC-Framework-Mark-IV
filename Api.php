f77a35e40f2e79b92bf385ab0e3efdef3a61fd10
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-07-17 22:05:33 +0200

    Initial Commit

---
+++ Api.php
@@ -0,0 +1,21 @@
+<?php
+namespace MOC\IV;
+
+class Api {
+
+	public static function getCore(){
+
+	}
+
+	public static function getModule(){
+
+	}
+
+	public static function getExtension(){
+
+	}
+
+	public static function getPlugin(){
+
+	}
+}
