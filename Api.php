4bdfa7d767066fa4bd772ef9775cced08905a3f6
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-07-18 12:49:47 +0200

    Clean Up & Documentation

--- Api.php
+++ Api.php
@@ -6,24 +6,27 @@ interface IApi {
 	public static function useModule();
 	public static function useExtension();
 	public static function usePlugin();
+
+	public static function loadClass( $Class );
+	public static function registerAutoClassLoader();
 }
 
 class Api implements IApi {
 
-	private static function loadClass( $Class ) {
+	final public static function loadClass( $Class ) {
 		$Class = trim( str_replace( __NAMESPACE__, '', $Class ), '\\' );
 		$Class = str_replace( array('\\','/'), DIRECTORY_SEPARATOR, __DIR__.DIRECTORY_SEPARATOR.$Class.'.php' );
 		if( false === ( $Class = realpath( $Class ) ) ) {
 			return false;
 		} else {
 			/** @noinspection PhpIncludeInspection */
-			require_once( $Class );
+			require( $Class );
 			return true;
 		}
 	}
 
-	public static function registerLoader() {
-		spl_autoload_register( array(__CLASS__,'loadClass') );
+	final public static function registerAutoClassLoader() {
+		spl_autoload_register( function($Class){ Api::loadClass($Class); } );
 
 		if( function_exists( 'xdebug_disable' ) ) { xdebug_disable(); }
 		error_reporting(0);
@@ -33,21 +36,21 @@ class Api implements IApi {
 		self::useCore()->useError()->registerType( self::useCore()->useError()->getType()->useShutdown() );
 	}
 
-	public static function useCore(){
+	final public static function useCore(){
 		return new Api\Core();
 	}
 
-	public static function useModule(){
+	final public static function useModule(){
 		return new Api\Module();
 	}
 
-	public static function useExtension(){
+	final public static function useExtension(){
 		return new Api\Extension();
 	}
 
-	public static function usePlugin(){
+	final public static function usePlugin(){
 		return new Api\Plugin();
 	}
 }
 
-Api::registerLoader();
+Api::registerAutoClassLoader();
--- README.md
+++ README.md
@@ -5,9 +5,32 @@ The easy way to OOP
 Mind the task - forget the obstacles...
 ---------------------------------------
 
+- PHP 5.3.0
+
 ------------------------------------------------------------------------------------------------------------------------
 
 Please take into account that current versions of MOC-Framework and plugins could contain serious bugs.
 Don't use them in production environments.
 
+Be also aware that current interfaces may change rapidly
+
 [![SensioLabsInsight](https://insight.sensiolabs.com/projects/708afcd6-5202-4d62-8a8b-99d68cae5d9b/big.png)](https://insight.sensiolabs.com/projects/708afcd6-5202-4d62-8a8b-99d68cae5d9b)
+
+Usage
+-----
+
+1. Get in the MOC
+
+	```php
+	require('Api.php');
+	```
+2. Start your engine
+
+	```php
+	use MOC\IV\Api;
+	```
+3. Put the pedal to the metal.
+
+	```php
+	print Api::useModule()->useEncoding()->useText('Latin-Chars öäüß')->getTextConvertedToUtf8();
+	```
