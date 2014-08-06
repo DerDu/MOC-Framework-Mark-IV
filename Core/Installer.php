1d6b96da81196420905de7d3dce544a788123b4b
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-08-07 00:22:18 +0200

    Core: Add Installer

---
+++ Core/Installer.php
@@ -0,0 +1,20 @@
+<?php
+namespace MOC\IV\Core;
+
+/**
+ * Interface IInstallerInterface
+ *
+ * @package MOC\IV\Core
+ */
+interface IInstallerInterface {
+
+}
+
+/**
+ * Class Installer
+ *
+ * @package MOC\IV\Core
+ */
+class Installer implements IInstallerInterface {
+
+}
