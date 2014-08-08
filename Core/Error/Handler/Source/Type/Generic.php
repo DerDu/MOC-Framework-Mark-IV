25170c8099dab21b749f6f8ba31252d321d86d19
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-08-08 22:58:14 +0200

    Core: Improved Error

--- Core/Error/Handler/Source/Type/Generic.php
+++ Core/Error/Handler/Source/Type/Generic.php
@@ -58,7 +58,7 @@ abstract class Generic implements IGenericInterface {
 	protected function getTrace() {
 
 		$Exception = new \Exception();
-		$Trace = explode( "\n", $Exception->getTraceAsString() );
+		$Trace = explode( "\n", trim( $this->getExceptionTraceAsString( $Exception ) ) );
 		$Length = count( $Trace );
 		$Result = array();
 		for( $Run = 0; $Run < $Length; $Run++ ) {
@@ -66,5 +66,47 @@ abstract class Generic implements IGenericInterface {
 		}
 
 		return '<ul><li>'.implode( '</li><li>', $Result ).'</li><ul>';
+	}
+
+	/**
+	 * @param \Exception $Exception
+	 *
+	 * @return string
+	 */
+	private function getExceptionTraceAsString( \Exception $Exception ) {
+		$Return = "";
+		$Count = 0;
+		foreach( $Exception->getTrace() as $Frame ) {
+			$ArgumentList = "";
+			if( isset( $Frame['args'] ) ) {
+				$ArgumentList = array();
+				foreach( $Frame['args'] as $Argument ) {
+					if( is_string( $Argument ) ) {
+						$ArgumentList[] = "'".$Argument."'";
+					} elseif( is_array( $Argument ) ) {
+						$ArgumentList[] = "Array";
+					} elseif( is_null( $Argument ) ) {
+						$ArgumentList[] = 'NULL';
+					} elseif( is_bool( $Argument ) ) {
+						$ArgumentList[] = ( $Argument ) ? "true" : "false";
+					} elseif( is_object( $Argument ) ) {
+						$ArgumentList[] = get_class( $Argument );
+					} elseif( is_resource( $Argument ) ) {
+						$ArgumentList[] = get_resource_type( $Argument );
+					} else {
+						$ArgumentList[] = $Argument;
+					}
+				}
+				$ArgumentList = join( ", ", $ArgumentList );
+			}
+			$Return .= sprintf( "#%s %s(%s): %s(%s)\n",
+				$Count,
+				isset( $Frame['file'] ) ? $Frame['file'] : 'unknown file',
+				isset( $Frame['line'] ) ? $Frame['line'] : 'unknown line',
+				( isset( $Frame['class'] ) ) ? $Frame['class'].$Frame['type'].$Frame['function'] : $Frame['function'],
+				$ArgumentList );
+			$Count++;
+		}
+		return $Return;
 	}
 }
