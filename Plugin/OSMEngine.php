7593193afb65ac41f0e302cbe1f365c2ef25a0a2
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-09-09 13:51:23 +0200

    Plugin: Improved OSMEngine

--- Plugin/OSMEngine.php
+++ Plugin/OSMEngine.php
@@ -1,6 +1,7 @@
 <?php
 namespace MOC\IV\Plugin;
 
+use MOC\IV\Api;
 use \MOC\IV\Core\Drive\File\IApiInterface as File;
 use MOC\IV\Core\Xml\Reader\Source\Node;
 use MOC\IV\Plugin\OSMEngine\Api\Element;
@@ -13,6 +14,7 @@ interface IOSMEngineInterface {
 class OSMEngine implements IOSMEngineInterface {
 
 	public function apiParser( File $OSMFile ) {
+
 		return new Parser( $OSMFile );
 	}
 
