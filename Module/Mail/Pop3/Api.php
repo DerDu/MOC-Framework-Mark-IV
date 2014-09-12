1a43fb71b46710396505680824e13d89b10193e2
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-09-12 13:43:54 +0200

    Clean Up & Documentation

--- Module/Mail/Pop3/Api.php
+++ Module/Mail/Pop3/Api.php
@@ -10,4 +10,32 @@ use MOC\MarkIV\Module\Mail\IApiInterface;
  */
 class Api implements IApiInterface {
 
+	/**
+	 * @param string $Host
+	 * @param string $User
+	 * @param string $Password
+	 * @param int    $Port
+	 *
+	 * @return IApiInterface
+	 */
+	public function openConnection( $Host, $User, $Password, $Port = 25 ) {
+		// TODO: Implement openConnection() method.
+	}
+
+	public function apiAddress() {
+		// TODO: Implement apiAddress() method.
+	}
+
+	public function apiContent() {
+		// TODO: Implement apiContent() method.
+	}
+
+	public function sendMail() {
+		// TODO: Implement sendMail() method.
+	}
+
+	public function closeConnection() {
+		// TODO: Implement closeConnection() method.
+	}
+
 }
